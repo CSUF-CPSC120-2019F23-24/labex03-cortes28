9c2a4f1c2f8205590737535df249f5ac1827907a
cortes28 <cortes28@csu.fullerton.edu>
2019-09-07 13:49:15 -0700

    Made a calculator for height conversion

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,28 @@
 // This program calculates a person's height in feet (') and inches (").
+
+#include <iostream>
+
+int main()
+{
+
+//We will get the person's height in inches to feet.
+
+int height_in_inches;
+
+std::cout << "Please enter the person\'s hieght in inches:";
+
+//Getting the height of the person in inches.
+
+std::cin >> height_in_inches;
+
+int feet = height_in_inches / 12;
+int inches = height_in_inches % 12;
+
+// We get our final result.
+
+std::cout << "Your height in feet is " << feet << "\'" << inches << "\".\n";
+
+return 0;
+
+
+}
