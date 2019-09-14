d0eea96f1c999cfa5a3046cfe2066645de55877f
cortes28 <cortes28@csu.fullerton.edu>
2019-09-14 14:17:43 -0700

    set a formula for finding the amount of gallons of paint it will take  fixed the indentation

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -4,17 +4,23 @@
 
 int main()
 {
-  // one gallon of paint covers 340 square feet
-  int coverage = 340;
-   int height = 6;
-    int width = 100;
 
-  // calculate how many gallons of paint it will take to cover
-  // the area with 2 coats of paint
-  double area = height * width * 2;
-    double gallons = area / coverage;
+// one gallon of paint covers 340 square feet
 
-  std::cout << "It will take " << gallons << " gallons of paint.\n";
+int coverage = 340;
+int height = 6;
+int width = 100;
 
-  return 0;
+// calculate how many gallons of paint it will take to cover
+// the area with 2 coats of paint
+
+double area = height * width * 2;
+double gallons = area / coverage;
+
+std::cout << "It will take " << gallons << " gallons of paint.\n";
+
+
+return 0;
+
+
 }
