597b9503a447b5da34048cf46c2c1ab0de91dec9
cortes28 <cortes28@csu.fullerton.edu>
2019-09-14 14:28:26 -0700

    Made a calculator for height conversion AND indented the program for it to look nice

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -5,24 +5,23 @@
 int main()
 {
 
-//We will get the person's height in inches to feet.
+  //We will get the person's height in inches to feet.
 
-int height_in_inches;
+  int height_in_inches;
 
-std::cout << "Please enter the person\'s hieght in inches:";
+  std::cout << "Please enter the person\'s hieght in inches:";
 
-//Getting the height of the person in inches.
+  //Getting the height of the person in inches.
 
-std::cin >> height_in_inches;
+  std::cin >> height_in_inches;
 
-int feet = height_in_inches / 12;
-int inches = height_in_inches % 12;
+  int feet = height_in_inches / 12;
+  int inches = height_in_inches % 12;
 
-// We get our final result.
+  // We get our final result.
 
-std::cout << "Your height in feet is " << feet << "\'" << inches << "\".\n";
+  std::cout << "Your height in feet is " << feet << "\'" << inches << "\".\n";
 
-return 0;
-
+  return 0;
 
 }
